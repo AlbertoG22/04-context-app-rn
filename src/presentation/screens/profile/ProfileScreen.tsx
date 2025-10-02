165bba1b6d45684f14cf0754e80c4b2c70f17a4b
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-10-02 17:45:31 -0600

    state changes

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { styles } from '../../../config/app-theme';
 import { useProfileStore } from '../../store/profile-store';
 
@@ -7,11 +7,34 @@ export const ProfileScreen = () => {
 
   const name = useProfileStore( state => state.name );
   const email = useProfileStore( state => state.email );
+  const changeProfile = useProfileStore( state => state.changeProfile );
 
   return (
     <View style={ styles.container }>
       <Text style={ styles.tile }>{ name }</Text>
       <Text style={ styles.tile }>{ email }</Text>
+
+      <Pressable
+        onPress={ () => useProfileStore.setState({ name: 'Alberto García' }) }
+        style={ styles.primaryButton }
+      >
+        <Text>Cambiar nombre</Text>
+      </Pressable>
+
+      <Pressable
+        onPress={ () => useProfileStore.setState({ email: 'albertog@google.com' }) }
+        style={ styles.primaryButton }
+      >
+        <Text>Cambiar email</Text>
+      </Pressable>
+
+      <Pressable
+        onPress={ () => changeProfile( 'Peter Parker', 'spiderman@gmail.com' )}
+        style={ styles.primaryButton }
+      >
+        <Text>Regresar cambios</Text>
+      </Pressable>
+
     </View>
   );
 };
--- src/presentation/store/profile-store.ts
+++ src/presentation/store/profile-store.ts
@@ -1,12 +1,17 @@
-import { create } from "zustand";
-
+import { create } from 'zustand';
 
 export interface ProfileState {
     name: string;
     email: string;
+
+    changeProfile: (name: string, email: string) => void;
 }
 
-export const useProfileStore = create<ProfileState>()( (set) => ({
+export const useProfileStore = create<ProfileState>()( (set, get) => ({
     name: 'Peter Parker',
-    email: 'spiderman@gmail.com'
+    email: 'spiderman@gmail.com',
+
+    changeProfile: (name: string, email: string) => {
+        set({ name: name, email: email }) // redundante, sería set({ name, email })
+    },
 }) );
