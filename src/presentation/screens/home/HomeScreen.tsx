584f7d85f1d1e5f3969916367fa3003e24fd4679
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-10-02 18:06:53 -0600

    creating another store w/ Zustand

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import { styles } from '../../../config/app-theme';
 import { useProfileStore } from '../../store/profile-store';
+import { useCounterStore } from '../../store/counter-store';
 
 export const HomeScreen = () => {
 
   const name = useProfileStore( state => state.name );
   const email = useProfileStore( state => state.email );
+  const count = useCounterStore( state => state.count );
 
   return (
     <View style={ styles.container }>
       <Text style={ styles.tile }>{ name }</Text>
       <Text style={ styles.tile }>{ email }</Text>
+
+      <Text style={ styles.tile }>Count: { count }</Text>
     </View>
   );
 };
--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Pressable, Text, View } from 'react-native';
 import { styles } from '../../../config/app-theme';
+import { useCounterStore } from '../../store/counter-store';
+import { useNavigation } from '@react-navigation/native';
 
 export const SettingsScreen = () => {
+
+  const count = useCounterStore( state => state.count );
+  const incrementBy = useCounterStore( state => state.incrementBy );
+
+  const navigation = useNavigation();
+
+  useEffect( () => {
+    navigation.setOptions({
+      title: `Contador: ${ count }`,
+    });
+  }, [ count ]);
+
   return (
     <View style={ styles.container }>
-      <Text>SettingsScreen</Text>
+      <Text style={ styles.tile }>Count: { count }</Text>
+
+      <Pressable
+        onPress={ () => incrementBy(+1) }
+        style={ styles.primaryButton }
+      >
+        <Text>+1</Text>
+      </Pressable>
+
+      <Pressable
+        onPress={ () => incrementBy(-1) }
+        style={ styles.primaryButton }
+      >
+        <Text>-1</Text>
+      </Pressable>
     </View>
   );
 };
